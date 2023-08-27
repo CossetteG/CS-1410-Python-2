0c2d9bf0b73c7aacf0293f85f4aa41b44e49b184
Cossette Gomez <10838257@my.uvu.edu>
2023-08-26 21:47:05 -0600

    review part 1

---
+++ PythonPlayground.py
@@ -0,0 +1,10 @@
+# only python compatible code here
+#imma try to run python from here
+
+my_string = "Bruh"
+
+#print(my_string) 
+print(my_string.find("u"))
+
+#aight folks we gotta terminal
+#time to review the slides
