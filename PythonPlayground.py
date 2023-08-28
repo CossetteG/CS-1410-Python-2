9878384f8ca76546d40f10dbb3083a9cbccb5e8d
Cossette <10838257@my.uvu.edu>
2023-08-28 15:59:08 -0600

    review part 2

--- PythonPlayground.py
+++ PythonPlayground.py
@@ -4,7 +4,17 @@
 my_string = "Bruh"
 
 #print(my_string) 
-print(my_string.find("u"))
+# print(my_string.find("u"))
 
 #aight folks we gotta terminal
 #time to review the slides
+
+a = "hello"
+# print(a[::-2])
+
+list1 = [1,2,3]
+list2 = ["uno", "dos", "tres"]
+
+zip1 = list(zip(list1, list2))
+
+print(zip1)
