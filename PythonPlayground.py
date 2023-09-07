937733bb0a10c329908e8fa535410c72d0d874a1
Cossette <10838257@my.uvu.edu>
2023-09-07 17:11:13 -0600

    class 9/7

--- PythonPlayground.py
+++ PythonPlayground.py
@@ -1,9 +1,55 @@
+'''
 class Student:
-    pass 
+    """lol """
+    boob = "yes"
+    def __init__(self, grade="A"):
+        self.grade_ = grade
+    noob = "no"
 
 best_student = Student()
 best_student.first_name = "Cossette"
 best_student.last_name = "Gomez"
 
-print(best_student.first_name, best_student.last_name)
-print(best_student)
+Student.noob = "maybe"
+
+ok_student = Student()
+
+print(best_student.first_name, best_student.noob)
+print(ok_student.noob)
+'''
+'''
+Cossette Gomez
+<__main__.Student object at 0x00000138CE007D60>
+'''
+
+class Player:
+    """Simple player class"""
+    def __init__(self, pw="mushroom", health=100, score=0, level=1):
+        self.health = health
+        self.score = score
+        self.level = level
+        self.powerups = [pw]
+        
+    def print_player(self):
+        if self.health <=0:
+            print("dead lol")
+        else:
+           print(self.health)
+
+    def add_powerup(self, pu):
+        self.powerups.append(pu)
+
+    def print_powerups(self):
+        if len(self.powerups) ==0:
+            print("you are weak sauce")
+        print(self.powerups) 
+
+mario = Player() #the __init__ is called magically 
+mario.print_player()
+
+mario.add_powerup("mushroom")
+mario.add_powerup("star")
+print(mario.powerups)
+
+peach = Player("fire flower")
+peach.print_powerups() 
