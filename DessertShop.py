c504d518cc2155946089b30e38035110ad9625cf
Cossette <10838257@my.uvu.edu>
2023-09-29 16:50:22 -0600

    dessert shop init

---
+++ DessertShop.py
@@ -0,0 +1,20 @@
+"""
+Dessert Shop!!
+"""
+
+class Coffee:
+    _sizes = [12, 16, 24, 32]
+    _bases = ["pour over", "esspresso", "chai"]
+    _syrups = ["pumpkin", "caramel", "toffee", "raspberry", "vanilla"]
+    _milks = ["Whole", "2%", "Almond", "Oat"]
+
+    def __init__(self, size, temp, base, syrup, milk, whipped_cream):
+        self._size = size
+        
+
+
+class Bread:
+    pass 
+
+class Cookies:
+    pass
