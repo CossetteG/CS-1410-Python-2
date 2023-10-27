9bbdfa2790f14cfcd63a4d793c7a7064cdba1a21
Cossette Gomez <10838257@my.uvu.edu>
2023-10-26 21:04:15 -0600

    debugging

--- PythonPlayground.py
+++ PythonPlayground.py
@@ -1,68 +1,37 @@
-#Exam will be open for one week, opens tomorrow
-#25 multiple choice questions, 2 hour test 
+class NegativeNumberError(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
 
-#implement the abstract method ----------------------------------------------------------------------------------------------------------------------------------------------------------------aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+    def check(self, num):
+        if num < 0:
+            raise NegativeNumberError("no negs allowed")
+        else:
+            return num 
 
-from typing import Protocol, List, Union
+checker = NegativeNumberError()
+# help(Exception)
 
-class Drawable(Protocol):
-    def draw(self) -> None:
-        pass 
-    
-class Draggable(Protocol):
-    def drag(self) -> None:
-        pass
-    
-class Rectangle:
-    def __init__(self) -> None:
-        pass 
-    
-    def draw(self) -> None:
-        print("draw rectangle")
-        
-class Circle:
-    def __init__(self) -> None:
-        pass 
-    
-    def draw(self) -> None:
-        print("Draw a circle")
+# a= checker.check(-25)
 
-    # def drag(self) -> None:
-    #     print("Drag circle to a point")
+def square_root(num):
+    # if num < 0:
+    #     raise NegativeNumberError(";alskjf")
+    # num = checker.check(num)
+    return checker.check(num)**.5
+
+try:
+    num = float(input("Enter a number: "))
+    result = square_root(num)
+except NegativeNumberError as e:
+    print("bogwater")
+    print(e)
+else:
+    print(f"The square root of {num} is {result}")
 
 
-from abc import ABC, abstractmethod
 
-class Drawable_(ABC):
-    @abstractmethod
-    def draw(self) -> None:
-        pass
-    
-class Rectangle_(Drawable_):
-    def __init__(self) -> None:
-        pass 
-    
-    def draw(self) -> None:
-        print("draw rectangle")
-
-rectangle = Rectangle()
-circle = Circle()
-
-def draw_shapes(shapes: List[Drawable]) -> None:
-    for shape in shapes:
-        shape.draw()
-
-def draw_and_drag(shapes: List[Union[Drawable, Draggable]]):
-    for shape in shapes:
-        shape.draw()
-        shape.drag() 
-
-draw_shapes([rectangle])
-draw_and_drag([rectangle, circle])
-
-#if using it, @property (the getter/setter decorator) the decorator must be on 
-# both and functions the same 
-#if every method in a class is abstract and it has no implementation,
-#  we call it an interface 
-#Protocol is like interface for Python, since pythono supports multiple inheritance 
-#multiple interface is available in all programing languages, unlike ^
+total = 0
+for i in range(100):
+    s = i + 1
+    total += s
+    # print(total)
